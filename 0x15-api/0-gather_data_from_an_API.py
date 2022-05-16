8093f78750556231b0027d092694929cb043de41
Drew-24 <andrewwil1@hotmail.com>
2022-05-16 12:56:22 -0400

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module holds python script that returns information about an employee todo list
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/todos'
+    parameters = (('userId', argv[1]),)
+    request_todo = requests.get(url, params=parameters)
+    todos = request_todo.json()
+
+    url = 'https://jsonplaceholder.typicode.com/users'
+    parameters = (('id', argv[1]),)
+    request_user = requests.get(url, params=parameters)
+    user = request_user.json()
+
+    MyList = []
+    for task in todos:
+        if task['completed']:
+            MyList.append(task)
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        user[0]["name"], len(MyList), len(todos)))
+    if len(MyList) > 0:
+        for task in MyList:
+            print("\t{}".format(task['title']))
